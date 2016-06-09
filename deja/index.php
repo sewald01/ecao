d2bb7e87e0fb133d258f6716b68982e29ce94e20
sewald01 <sewald01@seattlecentral.edu>
2016-06-09 10:06:46 -0700

    Deja Image Fix
    Added /deja directory and deja.php file, fixed image link within
    deja.php.

---
+++ deja/index.php
@@ -0,0 +1,170 @@
+#!/usr/local/bin/php
+<?php
+require("/home/content/p/s/y/psychewa/html/includes/bv-library.php");
+
+printHeader("The D&eacute;j&agrave; Vu Series");
+?><style type="text/css">
+<!--
+.style5 {font-size: large}
+-->
+</style><br><br>
+
+<!--
+<center>
+	<img src="/images/neppe-coast-2.jpg" width="566" height="228" alt="Coast To Coast Specials" border=0>
+    </center>
+    <br><br>
+      <center>
+        <a href="/shop/catalog/">Offer extended. Requires the code, irrespective of your listening to Dr Neppe on Coast To Coast. <br>
+        Place an on-line order (use the special code of &quot;coasttocoast&quot; to obtain a further 20% purchase discount). </a>
+-->
+<div align="center"> 
+<table width="550" border="0">
+  <tr>
+    <td><strong><a href="#DV"><img src="http://www.pni.org/psych-of-deja-vu.jpg" alt="psych of deja vu" width="372" height="205" border="0" /></a></strong></td>
+    <td width="25">&nbsp;</td>
+    <td><img src="http://www.brainvoyage.com/images/neppe_small.jpg" alt="neppe" width="166" height="207" /></td>
+  </tr>
+</table>
+<p>&nbsp;</p>
+</div>
+
+<hr width="100%" size=1 noshade><br>
+
+<H1 class=title><a name="DV" id="DV"></a>The D&eacute;j&agrave; Vu Series </H1>
+<h2 class=title</h2>
+
+
+<table align="center" width="100%">
+
+ <tr height="16">
+    <td width="75%" valign="top" colspan=2>
+      <hr width="100%" size=1 noshade></td>
+  </tr>
+
+
+
+
+
+
+
+ <tr>
+    <td width="75%" valign="top">
+      <H1 class=title><a href="/shop/catalog/product_info.php?cPath=22&products_id=37" style="text-decoration: none";>D&eacute;j&agrave; Vu Trilogy, by Dr. Vernon Neppe. </a></H1><br><br>This is your opportunity to read the most important, key information on deja vu, written in easy to read language,  yet  by the world authority in the area, Professor Vernon Neppe. Dr Neppe realized the need to combine his three critically important books, available only now for the first time as
+The Deja Vu Trilogy  Series combines all and the best of the three latest key electronic books at a  one third off the individual purchase price  for all three new Deja vu E-Books: Deja vu: A second look AND Deja vu revisited  AND   Deja vu Glossary<br><br>This selection is offered at a twenty-dollar ($20) savings over individually purchased texts.
+</td>
+    <td ALIGN="center" width="25%"> <A HREF="/shop/catalog/product_info.php?cPath=22&products_id=37"><IMG SRC="/images/dvTrilogy.jpg" ALT="Book Picture" border="0"></A>
+      <p> <a href="/shop/catalog/product_info.php?cPath=22&products_id=37"><img src="/shop/catalog/includes/languages/english/images/buttons/small_view.gif"border=0></a>
+      </p></td>
+  </tr>
+
+ <tr height="16">
+    <td width="75%" valign="top" colspan=2>
+      <hr width="100%" size=1 noshade></td>
+  </tr>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ <tr>
+    <td width="75%" valign="top">
+      <H1 class=title><a href="http://www.brainvoyage.com/deja/ASL" style="text-decoration: none";>D&eacute;j&agrave; Vu A Second Look, by Dr. Vernon Neppe. </a></H1><br><br>The key, definitive, most up to date d&eacute;j&agrave; vu book.
+This has Vernon M Neppe, MD, PhD as the editor and major author.  Dr Neppe is the world authority on d&eacute;j&agrave; vu Subeditor is Dr Art Funkhouser of Switzerland, one of the world's leading experts in the area, and author of several chapters.
+This book should be read in conjunction with the classic D&eacute;j&agrave; Vu Revisited and using the D&eacute;j&agrave; Vu Glossary and Library.</td>
+    <td ALIGN="center" width="25%"> <A HREF="http://www.brainvoyage.com/deja/ASL"><IMG SRC="../images/DVASLFrontCoverRed.jpg" ALT="Book Picture" border="0"></A>
+      <p> <a href="http://www.brainvoyage.com/deja/ASL"><img src="/shop/catalog/includes/languages/english/images/buttons/small_view.gif"border=0></a>
+      </p></td>
+  </tr>
+
+ <tr height="16">
+    <td width="75%" valign="top" colspan=2>
+      <hr width="100%" size=1 noshade></td>
+  </tr>
+
+ <tr>
+    <td width="75%" valign="top">
+      <H1 class=title><a href="http://www.brainvoyage.com/deja/DVR" style="text-decoration: none";>D&eacute;j&agrave; Vu  Revisited, by Dr. Vernon Neppe. </a></H1><br><br>This is the long awaited and classic rewrite of Vernon Neppe's The Psychology of D&eacute;j&agrave; Vu.<br>This is a must read to understand the d&eacute;j&agrave; vu phenomenon.<br>The author, Vernon M Neppe, MD, PhD is the world authority on d&eacute;j&agrave; vu who demonstrates how d&eacute;j&agrave; vu can be subdivided into at least four major distinct subtypes.
+This book should be read in conjunction with the definitive D&eacute;j&agrave; Vu: A Second Look and using the D&eacute;j&agrave; Vu Glossary and Library.
+</td>
+    <td ALIGN="center" width="25%"> <A HREF="http://www.brainvoyage.com/deja/DVR"><IMG SRC="../images/DVRfrontCoverGreen.jpg" ALT="Book Picture" border="0"></A>
+      <p><a href="http://www.brainvoyage.com/deja/DVR"><img src="/shop/catalog/includes/languages/english/images/buttons/small_view.gif" border=0</a>
+      </p></td>
+  </tr>
+
+
+ <tr height="16">
+    <td width="75%" valign="top" colspan=2>
+      <hr width="100%" size=1 noshade></td>
+  </tr>
+
+
+
+ <tr>
+    <td width="75%" valign="top">
+      <H1 class=title><a href="http://www.brainvoyage.com/deja/DVGL" style="text-decoration: none";>D&eacute;j&agrave; Vu  Glossary and Library, by Dr. Vernon Neppe. </a></H1><br><br>This is the third electronic book in the triad of Dr Vernon Neppe's series.
+This is a reference work on the d&eacute;j&agrave; vu phenomenon discussing terminology and providing a bibliography of relevant articles, very difficult to find because of the accents on d&eacute;j&agrave; vu.
+The author, Vernon M Neppe, MD, PhD is the world authority on d&eacute;j&agrave; vu.
+This book should be read in conjunction with the  classic D&eacute;j&agrave; Vu Revisited and the definitive D&eacute;j&agrave; Vu: A Second Look.
+A special offer combination of the three is available.</td>
+    <td ALIGN="center" width="25%"> <A HREF="http://www.brainvoyage.com/deja/DVGL"><IMG SRC="../images/dvgl.jpg" ALT="Book Picture" border="0"></A>
+      <p> <a href="http://www.brainvoyage.com/deja/DVGL"><img src="/shop/catalog/includes/languages/english/images/buttons/small_view.gif"border=0></a>
+      </p></td>
+  </tr>
+
+
+
+ <tr height="16">
+    <td width="75%" valign="top" colspan=2>
+      <hr width="100%" size=1 noshade></td>
+  </tr>
+
+  <tr>
+    <td width="75%" valign="top">
+      <H1 class=title><a href="http://www.brainvoyage.com/deja/PDV" style="text-decoration: none";>The Psychology of D&eacute;j&agrave; Vu,  by Dr. Vernon Neppe. </a></H1><br><br>Possibly one of the most important collector's items of any existing current book.
+This was the first book on d&eacute;j&agrave; vu, written by the world authority, Vernon M Neppe, MD, PhD and  a classic.
+More important two major publishing errors make this book almost unique.
+It has already been auctioned for a substantial amount and will shortly go worldwide in auctions such as EBay or equivalent.</td>
+    <td ALIGN="center" width="25%"> <A HREF="http://www.brainvoyage.com/deja/PDV"><IMG SRC="../images/deja_thumb.jpg" ALT="Book Picture" border="0"></A>
+      <p> <a href="http://www.brainvoyage.com/deja/PDV"><img src="/shop/catalog/includes/languages/english/images/buttons/small_view.gif"border=0></a>
+      </p></td>
+  </tr>
+</table>
+<h1>Other D&eacute;j&agrave; Vu Links</h1><br />
+<p><A HREF="http://www.pni.org/books/deja_vu_info.html">
+                          D&eacute;j&agrave; Vu research and theory Vernon M. Neppe  </a>
+						</p>						<p>
+						<A HREF="http://www.pni.org/research/anomalous/deja/deja_manifestations.html">
+                          The Various Manifestations of D&eacute;j&agrave; Vu Experience</a>
+						</p>
+						<!--END NEW-->
+                        <p><A HREF="http://www.pni.org/neuropsychiatry/instruments/deja_vu.html">
+                          D&eacute;j&agrave; Vu Questionnaire</a><A HREF="http://www.pni.org/neuropsychiatry/instruments/deja_vu.html"></a></p>
+                        <p><A HREF="http://www.pni.org/books/deja_vu_info.html"> The Various Manifestations
+                          of D&eacute;j&agrave; Vu Experience</A> </p>
+                        <p><A HREF="http://www.pni.org/books/#the%20psychology%20of%20d%E9j%E0%20vu">
+                          The Psychology of D&eacute;j&agrave; Vu</A></p>
+                        <p><A HREF="http://www.pni.org/books/deja_vu_info.html">
+                          Neppe D&eacute;j&agrave; Vu Research and Theory</A></p>
+                        <p><A HREF="http://home.cc.umanitoba.ca/~mdlee/dejavu.htm">
+                          Funkhouser d&eacute;j&agrave; vu discussion</A></p>
+                          <p><A HREF="http://www.pni.org/research/anomalous/deja/">
+                          D&eacute;j&agrave; vu Perspective</A></p>
+<script type="text/javascript">
+<!--
+window.onload=openmenu('menuDVseries');
+//-->
+</script>                          
+<?php printFooter(); ?>
